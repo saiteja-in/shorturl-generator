7bbfcf36dcb75ebea954f8c64459acdadad37e1c
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-20 17:07:10 +0530

    created post request on contoller url.js

--- controllers/url.js
+++ controllers/url.js
@@ -1,1 +1,21 @@
 const {nanoid}=require('nanoid')
+const URL=require('../models/url')
+
+async function handleGenerateNewShortURL(req,res){
+     const shortID=nanoid(8)
+     const body=req.body;
+     if(!body.url)return res.status(400).json({
+        error:"url is required"
+     })
+     await URL.create({
+        shortID:shortID,
+        redirectURL:body.url,
+        visitedHistory:[]
+     })
+     return res.json({
+        id:shortID
+     })
+}
+module.exports={
+    handleGenerateNewShortURL,
+}
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port=8001
 
 app.listen(port,()=>{
     console.log(`server started with port ${port}`)
-})
+})  
