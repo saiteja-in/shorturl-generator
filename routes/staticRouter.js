64f439530f1b1291315f59dbc3b6aa33c113739a
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-22 11:20:04 +0530

    modified

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,6 +1,10 @@
+const URL=require('../models/url')
 const express = require("express");
 const router = express.Router();
-router.get('/',(req,res)=>{
-    res.render("home")
+router.get('/',async(req,res)=>{
+    const allUrls=await URL.find({})
+    return res.render("home",{
+        urls:allUrls
+    })
 })
 module.exports = router;
